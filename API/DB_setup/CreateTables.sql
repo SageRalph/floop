65d802f02c8b17d73e7403ba0c0294f34d6ac96f
David Ralph <David.M.L.Ralph@gmail.com>
2015-08-25 02:34:30 +0100

    DDL and test data for film and user details
    Partial implementation of issues #7 and #8.
    Fixed 'Embed trailers' button text not changing.

--- API/DB_setup/CreateTables.sql
+++ API/DB_setup/CreateTables.sql
@@ -1,12 +1,23 @@
 CREATE TABLE Account (
 username    VARCHAR(30) PRIMARY KEY,
-regDate     DATETIME    DEFAULT CURRENT_TIMESTAMP
+regDate     DATETIME    DEFAULT CURRENT_TIMESTAMP,
+email       VARCHAR(254),
+phone       VARCHAR(16),
+studentNo   VARCHAR(6)
 );
 CREATE TABLE Film (
 filmID      INTEGER	AUTO_INCREMENT	PRIMARY KEY,
 title       VARCHAR(30)	NOT NULL,
 trailer     VARCHAR(50),
+releaseYear INTEGER,
 watched     BOOLEAN     NOT NULL
+);
+CREATE TABLE FilmGenre (
+filmID      INTEGER,
+genre       VARCHAR(30),
+INDEX	(filmID),
+CONSTRAINT  PK_FilmGenres      PRIMARY KEY(filmID, genre),
+CONSTRAINT  FK_FilmGenreID     FOREIGN KEY(filmID)	REFERENCES Film(filmID)
 );
 CREATE TABLE Rating (
 filmID      INTEGER,
--- API/DB_setup/TestData.sql
+++ API/DB_setup/TestData.sql
@@ -1,13 +1,19 @@
 -- username
-INSERT INTO Account (username) VALUES ('David');
-INSERT INTO Account (username) VALUES ('Josh');
-INSERT INTO Account (username) VALUES ('Ryan');
-INSERT INTO Account (username) VALUES ('Tom');
+INSERT INTO Account (username, email, phone, studentNo) VALUES ('David','test@email.com','1234567890','687691');
+INSERT INTO Account (username)  VALUES ('Josh');
+INSERT INTO Account (username)  VALUES ('Ryan');
+INSERT INTO Account (username)  VALUES ('Tom' );
 -- title, trailer, watched
-INSERT INTO Film (title, trailer, watched)  VALUES ('Terminator',           null,true);
-INSERT INTO Film (title, trailer, watched)  VALUES ('The imitation game',   null,false);
-INSERT INTO Film (title, trailer, watched)  VALUES ('Pans labrinth ',       'https://www.youtube.com/embed/O9Rdqm_74C0',false);
-INSERT INTO Film (title, trailer, watched)  VALUES ('Prometheus & Alien',   'https://www.youtube.com/embed/xSNxqgUdPBI',true);
+INSERT INTO Film (title, watched, trailer)  VALUES ('Pans labrinth ',       false,  'https://www.youtube.com/embed/O9Rdqm_74C0');
+INSERT INTO Film (title, watched, trailer)  VALUES ('Prometheus & Alien',   true,   'https://www.youtube.com/embed/xSNxqgUdPBI');
+INSERT INTO Film (title, watched)           VALUES ('Terminator',           true    );
+INSERT INTO Film (title, watched)           VALUES ('The imitation game',   false   );
+-- filmID, genre
+INSERT INTO FilmGenre VALUES (1,'Action');
+INSERT INTO FilmGenre VALUES (1,'Comedy');
+INSERT INTO FilmGenre VALUES (2,'Action');
+INSERT INTO FilmGenre VALUES (3,'Action');
+INSERT INTO FilmGenre VALUES (4,'Horror');
 -- filmID, username, rating
 INSERT INTO Rating VALUES (1,	'David',4);
 INSERT INTO Rating VALUES (1,	'Josh',	3);
